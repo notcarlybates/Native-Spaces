657ffe893c01f94dcb0e5a7e4bbd6a2cf52045e1
notcarlybates <carly30b@gmail.com>
2024-10-07 11:56:22 +0200

    info page

--- src/components/InfoPopup.tsx
+++ src/components/InfoPopup.tsx
@@ -68,40 +68,79 @@ const InfoPopup = () => {
                 <DialogContent dividers>
                     {showFullCredits ? (
                         <>
-                            <Typography variant={'h6'} gutterBottom>
+                            <Typography variant={'h5'} gutterBottom>
                                 <b>Credits</b>
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                The Native Space project was initiated by Elizabeth Solomon and Sarah Kanouse in collaboration with the Massachusett Tribe at Ponkapoag and the municipalities and organizations sponsoring audio content. Special thanks to the City of Salem, in particular Patti Kelleher and Elizabeth Peterson, for hosting the pilot chapter of this project. The title is inspired by Natchee Blu Barnd’s book <Link href="https://osupress.oregonstate.edu/book/native-space" target="_blank">Native Space</Link>, which argues that Indigenous communities use space even in colonized territories to reclaim their identities and assert an unbreakable relationship with land.
+                            Native Spaces was initiated by Elizabeth Solomon and Sarah Kanouse in collaboration with the Massachusett Tribe at Ponkapoag and the municipalities and organizations sponsoring audio content. The title is related to Natchee Blu Barnd’s book{" "}
+                            <Link href='https://osupress.oregonstate.edu/book/native-space' target='_blank'> Native Space</Link>, which argues that Indigenous communities use space even in colonized territories to reclaim their identities and assert an unbreakable relationship with land.
+
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                Music by Jennifer Kreisberg (Tuscarora) and members of the Massachusett Tribe at Ponkapoag.
+                            The medallion logo is adapted from a design by Sadie Red Wing (Spirit Lake Lakota). Music by the{' '}
+                            <Link href='https://www.facebook.com/RedHawkSingersAndDancers/' target='_blank'>Red Hawk Singers </Link> and Pomham Singers, recorded at the 2024 Veteran’s Powwow, with bass motifs by{' '}
+                            <Link href='http://erikmusic.net/about.htm' target='_blank'>Erik Kramer</Link>. 
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                Halsey Bergund developed the Roundware platform on which this project is built, and Carly Bates customized its code for Native Space. For full credits, including names of speakers, click the link for each location.
+                            Halsey Bergund developed the Roundware platform on which this project is built, and Carly Bates customized its code for Native Spaces.
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                Salem 2024
+                            The Native Spaces concept was developed with the support of a NEFA Collective Imagination for Spatial Justice grant and produced with funding from Northeastern’s NuLab for Digital Humanities and Computational Social Sciences. Special thanks to the City of Salem, especially Elizabeth Peterson, Julie Barry, and Patti Kelleher, for funding and hosting the pilot chapter of this project. 
+                            </Typography>
+
+                            <Typography variant={'h6'} gutterBottom>
+                                <b>Voices</b>
+                            </Typography>
+                            <Typography gutterBottom mb={3}>
+  Nehemiah Duarte<br />
+  Alysha Gray<br />
+  Elizabeth Gray<br />
+  Faries Gray<br />
+  Alex Green<br />
+  Thomas Green<br />
+  Marie Hunt<br />
+  Patti Kelleher<br />
+  Robert Nichols<br />
+  Jeanne Oliver-Foster<br />
+  Elizabeth Peterson<br />
+  Alyssa Rosemartin<br />
+  Elizabeth Solomon<br />
+  And other members of the Massachusett Tribe
+</Typography>
+<Typography gutterBottom mb={3}>
+                            Original recordings by Sarah Kanouse, Nicholas Brown, Jimmy Bautista, and Nathen Green. Additional field recordings by{' '}
+                            <Link href="https://freesound.org/people/kantoesploras/" target="_blank">kantoesploras</Link>
+                            , including “Common Raven, Mt. Greylock,” “Desert Natural Area,” and “Eastern Whip-poor-will” from FreeSound.org used under a Creative Commons Attribution NonCommercial 4.0 license
+
                             </Typography>
                         </>
                     ) : (
                         <>
-                            <Typography variant={'h6'} gutterBottom>
+                            <Typography variant={'h5'} gutterBottom>
                                 <b>Welcome to Native Space</b>
-								</Typography>
-								<Typography gutterBottom mb={3}>
+                                </Typography>
+                                <Typography gutterBottom mb={3}>
 
-                                You are in Native Space, on the traditional and ancestral lands of the Massachusett Tribe. Our ancestors have been here for at least 10,000 years. We are still here, and we are still the holders of the land.
+                                You are in Native space, a visitor to the homelands of the Massachusett Tribe. Our ancestors have been here for at least 10,000 years, and the land resonates with the stories of our ancestors, the songs of our elders, and the voices of our youth. 
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                Five hundred years ago, colonists began arriving, and the landscape and our way of life changed. But regardless of what has happened to the land, it is still Native Space, and it still resonates with the stories of our ancestors, the songs of our elders, and the voices of our youth.
+                            If you’re on your phone and in a place where audio is available, press play on the next screen and keep this website open as you move through space to listen to an ever-changing sound collage that responds to your location.
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                This website shares stories and perspectives about this land from present-day members of the Massachusett Tribe, as well as other people, both Native and non-Native, who have come to call this place home.
+                            If you’re off-site, click the map icon to explore audio content in your web browser. 
                             </Typography>
                             <Typography gutterBottom mb={3}>
-                                Beginning in Salem in 2024, we are releasing non-linear, place-based audio stories that you can listen to while walking in the areas indicated on the map, or access via the website’s map view off-site. Whether you are on the ground or away, Native Space will create a unique sound collage highlighting the Indigenous histories, ideas, and experiences that are rooted in this place and continue to animate the landscape today.
+                            Whether you’re on the ground or away, Native Spaces will create a unique sonic experience highlighting perspectives on this land from present-day members of the Massachusett Tribe, as well as other people, both Native and non-Native, who have come to call this place home.
+                                                         </Typography>
+                                                         <Typography variant={'h6'} gutterBottom>
+                            <b>Accessibility</b>
                             </Typography>
+                            <Typography gutterBottom mb={3}>
+
+                            To read a transcript of voice clips, click on the placemark or open the history panel. 
+
+                            </Typography>
+                            
                             <Divider style={{ marginBottom: '16px', backgroundColor: 'rgba(0, 0, 0, 0.12)' }} />
                             <Typography variant={'body2'} color='primary' onClick={handleShowFullCredits} style={{ cursor: 'pointer', fontSize: '1.2em' }}>
                                 Full Credits
@@ -120,3 +159,4 @@ const InfoPopup = () => {
 };
 
 export default InfoPopup;
+
